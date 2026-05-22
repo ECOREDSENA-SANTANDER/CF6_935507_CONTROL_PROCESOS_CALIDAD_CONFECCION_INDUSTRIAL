c947dbbd3409a08394b72c4b02fabf7ac382f085
lizmanchego <karinamanchego0416@gmail.com>
2026-05-22 15:59:41 -0500

    [ADD] Tema 1.1

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -6,18 +6,126 @@
         .titulo-principal__numero
           span 1
         h1 Control de calidad en los procesos de confección e insumos
+      
+      figure
+        .video
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/aZex9L1wlmA?si=SFIYLfh2OjGY8LfB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      figcaption.mb-5 Video. Inspección producto en proceso - Área de patronaje y corte
+
+      
+      
+      p.mb-0 Al momento de estructurar un sistema de control de calidad, se parte de el diagrama operativo de la prenda a confeccionar, de forma que se tiene una visión global del proceso y a partir de ello se crea la estrategia de control. Para ello, se deben definir los puntos y las normas de inspección y la periodicidad de esta.
+      
+      
+      
       Separador
       #t_1_1.titulo-segundo.color-acento-contenido(data-aos='fade-right')
         h2 1.1 Control de calidad en patronaje
+      
+      .row.justify-content-center.mb-5.align-items-end(data-aos='fade-right')
+        .col-sm-12.col-lg-3.d-none.d-lg-block
+          img(src='@/assets/curso/temas/t1/2.png', alt='')
+        .col-sm-12.col-lg-8
+          p.mb-3 El control de los procesos es comprender cuales son las variaciones que se pueden presentar en las características del producto, encontrar las causas y tomar las decisiones para controlar la variabilidad, eliminar los errores, estandarizando el proceso mediante la aplicación de técnicas que permitan establecer mejoras y así controlar la variabilidad.
+          p.mb-4 En el proceso de patronaje la persona responsable del proceso junto con el auditor de calidad, realizan la revisión del paquete físico de moldes o patrones que comprenden la pieza con el fin de garantizar la calidad de los mismos, es importante revisarlos a pesar de que se hayan desarrollado el patronaje de la prenda con algún software especializado.
+          .row.justify-content-center.mb-0
+            .col-sm-12.col-lg-7
+              .bg-color-2.p-4
+                p.mb-0.text-white Una vez se desarrolla la prenda la validación de los patrones debe estar contenida en un documento de revisión de patronaje, lo que permite la trazabilidad del proceso.
+                p.mb-0.text-white Los productos tienen variables y atributos, las variables son características cuantitativas y los atributos son las características no cuantificables, los cuales unidos son los requisitos de una prenda.
+            .col-sm-12.col-lg-5.d-none.d-lg-block.bg-color-5
+              img(src='@/assets/curso/temas/t1/1.svg', alt='')
+      p.mb-5 Los requisitos además están clasificados de la siguiente manera:
+      SlyderF.mb-5(columnas="col-lg-6 col-xl-4")
+        .tarjeta.bg-color-6.p-4
+          .row.justify-content-center.mb-4
+            .col-5
+              img(src='@/assets/curso/temas/t1/3.svg', alt='')
+          h4.text-center Explícitos
+          p.mb-0 Declarados por el cliente, según sus expectativas.
+        .tarjeta.bg-color-6.p-4
+          .row.justify-content-center.mb-4
+            .col-5
+              img(src='@/assets/curso/temas/t1/4.svg', alt='')
+          h4.text-center Implícitos
+          p.mb-0 Necesarios para que cumplan la función para la que son creados.
+        .tarjeta.bg-color-6.p-4
+          .row.justify-content-center.mb-4
+            .col-5
+              img(src='@/assets/curso/temas/t1/5.svg', alt='')
+          h4.text-center Legales o reglamentarios
+          p.mb-0 De acuerdo con la normatividad vigente.
+        .tarjeta.bg-color-6.p-4
+          .row.justify-content-center.mb-4
+            .col-5
+              img(src='@/assets/curso/temas/t1/6.svg', alt='')
+          h4.text-center Empresariales
+          p.mb-0 Según las políticas de producción de la empresa.
+      
+      
+      p.mb-5 Los requisitos que se deben considerar en el proceso de patronaje son:
+      .row.justify-content-center.mb-5(data-aos='fade-right')
+        .col-sm-12.col-lg-5.d-none.d-lg-block
+          img(src='@/assets/curso/temas/t1/7.png', alt='')
+        .col-sm-12.col-lg-7
+          LineaTiempoD.color-acento-contenido
+            .row(numero="1" titulo="Medidas del patrón con y sin costuras")
+              .col-12
+                p.mb-0 Se comparan las medidas iniciales con las de la prenda terminada con el fin de observar que corresponda con la talla, además es necesario revisar que las medidas de las piezas sean iguales al unir y tengan simetría; en este proceso se pueden superponer las piezas para observar que coincidan en cuanto a sus longitudes.
+            
+            .row(numero="2" titulo="Cantidad de piezas")
+              .col-12
+                p.mb-0 Se revisa la correspondencia de piezas respecto al diseño y al dibujo plano del patrón.
+            .row(numero="3" titulo="Señalización de pieza")
+              .col-12
+                p.mb-0 Se debe identificar cada parte de la prenda de forma que no se preste a dualidades de información, además de contener la cantidad de piezas a cortar para el producto solicitado.
+            .row(numero="4" titulo="Aplomos")
+              .col-12
+                p.mb-0 Observar que exista una indicación clara de las líneas de aplomo de cada pieza.
+            .row(numero="5" titulo="Costuras")
+              .col-12
+                p.mb-0 Verificar los márgenes de costura en cada pieza, y la correspondencia con la pieza a unir; por ejemplo, si en el hombro en la pieza del delantero el ancho es de 1 cm de margen de costura, en el hombro posterior debe corresponder a la misma medida de margen de costura.
+            .row(numero="6" titulo="Piquetes y puntos")
+              .col-12
+                p.mb-0 Comprobar la existencia de puntos y piquetes, alineando las piezas y correspondencia entre la unión de puntos y piquetes.
+            .row(numero="7" titulo="Plantillas")
+              .col-12
+                p.mb-0 Examinar la funcionalidad de la plantilla y sus medidas.
+
+
       Separador
       #t_1_2.titulo-segundo.color-acento-contenido(data-aos='fade-right')
         h2 1.2 Control de calidad en trazo y corte
+      
+      
+      
+      
+      
+      
+      
+      
       Separador
       #t_1_3.titulo-segundo.color-acento-contenido(data-aos='fade-right')
         h2 1.3 Control de calidad en confección
+      
+      
+      
+      
+      
+      
+      
+      
       Separador
       #t_1_4.titulo-segundo.color-acento-contenido(data-aos='fade-right')
         h2 1.4 Control de calidad en terminados
+      
+      
+      
+      
+      
+      
+      
+      
       Separador
       #t_1_5.titulo-segundo.color-acento-contenido(data-aos='fade-right')
         h2 1.5 Control de calidad en insumos, materiales y textiles
