d5f666d7b60f8b23954598ae66e0cfc37d9c4a91
lizmanchego <karinamanchego0416@gmail.com>
2026-06-12 09:46:04 -0500

    [UPD] Cargo Johana

--- src/config/global.js
+++ src/config/global.js
@@ -292,7 +292,7 @@ export default {
         {
           nombre: 'Claudia Johanna Gómez Pérez',
           cargo:
-            'Responsable del Ecosistema de Recursos Educativos Digitales (RED)',
+            'Profesional 06. Responsable Ecosistema Virtual de Recursos Educativos Digitales',
           centro: 'Centro Agroturístico - Regional Santander',
         },
         {
