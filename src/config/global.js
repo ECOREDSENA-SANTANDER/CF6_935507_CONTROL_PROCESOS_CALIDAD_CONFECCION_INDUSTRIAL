890424e43cd0be93504e4d8368f8d53541658552
lizmanchego <karinamanchego0416@gmail.com>
2026-09-14 14:52:20 -0500

    ADD CFA

--- src/config/global.js
+++ src/config/global.js
@@ -299,7 +299,7 @@ export default {
         {
           nombre: 'Claudia Johanna Gómez Pérez',
           cargo:
-            'Profesional 06. Responsable Ecosistema Virtual de Recursos Educativos Digitales',
+            'Profesional G06. Responsable Ecosistema Virtual de Recursos Educativos Digitales',
           centro: 'Centro Agroturístico - Regional Santander',
         },
         {
